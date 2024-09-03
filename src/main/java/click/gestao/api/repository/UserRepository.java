19e3e7686c2312b740cf31a415a4bd61b9ba7d06
giovaniocan <giovaniocan@gmail.com>
2024-09-03 17:08:05 -0300

    user repository and authentication service class to get the users params

--- src/main/java/click/gestao/api/repository/UserRepository.java
+++ src/main/java/click/gestao/api/repository/UserRepository.java
@@ -2,6 +2,8 @@ package click.gestao.api.repository;
 
 import click.gestao.api.domain.user.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
---
+++ src/main/java/click/gestao/api/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package click.gestao.api.service;
+
+import click.gestao.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//se a classe tiver esse @Service e implementar UserDetailsService, o spring vai implementar ela sozinha, nao precisa chamar em nenhum lugar
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
