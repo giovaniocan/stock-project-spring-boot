0fc56784c493e0606627f36a9cef7e36d71c8ffd
giovaniocan <giovaniocan@gmail.com>
2024-09-04 14:17:12 -0300

    when user authenticates create the jwt token

--- src/main/java/click/gestao/api/controller/AuthenticationController.java
+++ src/main/java/click/gestao/api/controller/AuthenticationController.java
@@ -1,6 +1,8 @@
 package click.gestao.api.controller;
 
 import click.gestao.api.domain.user.DadosAuthentication;
+import click.gestao.api.domain.user.Usuario;
+import click.gestao.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,17 +20,15 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DadosAuthentication data){
-        //aqui não podemos apenas chamar a nossa service, e sim usar um metodo nativo do spring e eleque vai chamar a nosas service e autenticar
-
-        //criando um objeto do tipo que o authentication manager precisa
         var token = new UsernamePasswordAuthenticationToken(data.login(), data.senha());
-
-       //quando isso é chamado, o spring chama o metodo loadUserByUsername da nossa classe AuthenticationService, e usa o repository para buscar o usuario
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 }
---
+++ src/main/java/click/gestao/api/infra/security/TokenService.java
@@ -0,0 +1,38 @@
+package click.gestao.api.infra.security;
+
+import click.gestao.api.domain.user.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    public String gerarToken(Usuario usuario){
+        try {
+            //esse algoritmo é o que vai ser usado para criptografar o token, e esse secret é uma chave que a gente passa para garantir que o token é valido
+
+            var algorithm = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API de teste da Click")
+                    // aqui serve para guardar as informacoes do usuario no token, no caso só estamos guardando quem é o usuario
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId()) // aqui podemos armazenar mais informações, e podemos fazer isso quantas vezes forem necessarias, em uma estrutura de chave e valor
+                    .withExpiresAt(dataExpiaracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao criar token", exception);    }
+    }
+
+    // aqui estamos criando e retornando quando o token vai expirar
+    private Instant dataExpiaracao() {
+        return LocalDateTime.now().plusHours(12).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
